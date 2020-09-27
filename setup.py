b71e371121adf11ccc2765a5fbad66cb982b383f
yangjian <yangjian@zetyun.com>
2020-09-27 19:22:03 +0800

    Update README.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 from setuptools import find_packages
 from setuptools import setup
 
-version = '0.1.11'
+version = '0.1.12'
 
 requirements = [
     'scipy>=1.3.1',
