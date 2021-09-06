22a08b8848ed162f1ce0fa204e75afbf1a68f16d
lixfz <lixf@sovon.net>
2021-09-06 17:32:57 +0800

    Update test cases

--- deeptables/tests/models/run_dt.py
+++ deeptables/tests/models/run_dt.py
@@ -1,26 +1,46 @@
+import os
+import numpy as np
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 from deeptables.datasets import dsutils
 from deeptables.models import deeptable, deepnets
 
-# loading data
-df = dsutils.load_bank()
-df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
 
-y = df_train.pop('y')
-y_test = df_test.pop('y')
+def run(distribute_strategy=None, batch_size=32, epochs=5):
+    # loading data
+    df = dsutils.load_bank()
+    df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
 
-# training
-config = deeptable.ModelConfig(nets=deepnets.DeepFM, earlystopping_patience=999,apply_class_weight=True)
-dt = deeptable.DeepTable(config=config)
-model, history = dt.fit(df_train, y, batch_size=32, epochs=5)
+    y = df_train.pop('y')
+    y_test = df_test.pop('y')
 
-# evaluation
-result = dt.evaluate(df_test, y_test, verbose=0)
-print('score:', result)
+    # training
+    config = deeptable.ModelConfig(nets=deepnets.DeepFM, earlystopping_patience=999, apply_class_weight=True,
+                                   distribute_strategy=distribute_strategy, )
+    dt = deeptable.DeepTable(config=config)
+    model, history = dt.fit(df_train, y, batch_size=batch_size, epochs=epochs)
 
-# scoring
-preds = dt.predict(df_test)
+    # evaluation
+    result = dt.evaluate(df_test, y_test, verbose=0)
+    print('score:', result)
 
-#
-print('done')
+    # scoring
+    preds = dt.predict(df_test)
+    uniques = np.unique(preds, return_counts=True)
+    print({k: v for k, v in zip(*uniques)})
+
+
+if __name__ == '__main__':
+    gpus = tf.config.list_physical_devices('GPU')
+    bs = int(os.environ.get('BATCH_SIZE', '32'))
+    es = int(os.environ.get('EPOCHS', '5'))
+
+    if len(gpus) < 2:
+        run(batch_size=bs, epochs=es)
+    else:
+        strategy = tf.distribute.MirroredStrategy()
+        # with strategy.scope():
+        run(distribute_strategy=strategy, batch_size=len(gpus) * bs, epochs=es)
+
+    print('done')
--- deeptables/tests/models/run_dt_with_dask.py
+++ deeptables/tests/models/run_dt_with_dask.py
@@ -1,11 +1,15 @@
+import os
+
+import tensorflow as tf
+
 from deeptables.datasets import dsutils
 from deeptables.models import deeptable, deepnets
 from hypernets.tabular import dask_ex as dex
-from hypernets.tests.tabular.dask_transofromer_test import setup_dask
 from hypernets.tabular.metrics import calc_score
+from hypernets.tests.tabular.dask_transofromer_test import setup_dask
 
 
-def run():
+def run(distribute_strategy=None, batch_size=32, epochs=5):
     # loading data
     df = dsutils.load_bank_by_dask()
     df_train, df_test = dex.train_test_split(df, test_size=0.2, random_state=42)
@@ -14,9 +18,10 @@ def run():
     y_test = df_test.pop('y')
 
     # training
-    config = deeptable.ModelConfig(nets=deepnets.DeepFM, earlystopping_patience=5)
+    config = deeptable.ModelConfig(nets=deepnets.DeepFM, earlystopping_patience=5,
+                                   distribute_strategy=distribute_strategy, )
     dt = deeptable.DeepTable(config=config)
-    model, history = dt.fit(df_train, y, batch_size=32, epochs=30)
+    model, history = dt.fit(df_train, y, batch_size=batch_size, epochs=epochs)
 
     # save
     model_path = 'model_by_dask'
@@ -30,11 +35,21 @@ def run():
     print('score:', result)
 
     # scoring
-    preds = dt2.predict(df_test)
-    proba = dt2.predict_proba(df_test)
+    preds = dt2.predict(df_test, batch_size=512, )
+    proba = dt2.predict_proba(df_test, batch_size=512, )
     print(calc_score(y_test, preds, proba, metrics=['accuracy', 'auc']))
 
 
 if __name__ == '__main__':
     setup_dask(None)
-    run()
+    gpus = tf.config.list_physical_devices('GPU')
+    bs = int(os.environ.get('BATCH_SIZE', '32'))
+    es = int(os.environ.get('EPOCHS', '5'))
+
+    if len(gpus) < 2:
+        run(batch_size=bs, epochs=es)
+    else:
+        strategy = tf.distribute.MirroredStrategy()
+        run(distribute_strategy=strategy, batch_size=len(gpus) * bs, epochs=es)
+
+    print('done')
